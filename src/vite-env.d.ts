b77f1e0ecc3a886a02eb709cdbd19d2b372a0ce3
Niraj Chauhan <mail.nirajchauhan@gmail.com>
2025-06-03 11:44:12 +0530

    feat(env): ✨ add environment variables for Ansible integration
    * Introduced `.env-copy` file with necessary Ansible API configurations.
    * Updated `vite-env.d.ts` to include new environment variables.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_SITERELIC_API_URL: string;
+  readonly VITE_SITERELIC_API_KEY: string;
+  readonly VITE_ANSIBLE_API_URL: string;
+  readonly VITE_ANSIBLE_USERNAME: string;
+  readonly VITE_ANSIBLE_TOKEN: string;
+  readonly VITE_REQUIRED_A_RECORD: string; // Add this line
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
